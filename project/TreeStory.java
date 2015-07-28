230ddb3137f676f63e1d69c7c7bb791fcc554844
wao1987 <wilsonordonez@wilsononezsimac.home>
2015-07-27 22:04:27 -0400

    Java Testing

---
+++ project/TreeStory.java
@@ -0,0 +1,24 @@
+import java.io.Console;
+ 
+public class TreeStory {
+    
+    public static void main(String[] args) {
+        Console console = System.console();
+        /*  Some terms:
+            noun - Person, place or thing
+            verb - An action
+            adjective - A description used to modify or describe a noun
+            Enter your amazing code here!
+        */
+       String name = console.readLine("Enter a name:  ");
+       String adjective = console.readLine("Enter an adjective:  ");
+       String noun = console.readLine("Enter a noun:  ");
+       String adverb = console.readLine("Enter an adverb:  ");
+       String verb = console.readLine("Enter a verb ending with -ing:  ");
+      
+       console.printf("Your TreeStory:\n---------\n");
+       console.printf("%s is a %s.  ", name, adjective, noun);
+       console.printf("They are always %s %s. \n", adverb, verb);
+    }
+    
+}
